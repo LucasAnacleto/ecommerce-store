683b8c51be184a66f6f95b6ffef060fdc892ebac
lucas.anacleto <lucas.anacleto@outlook.com>
2023-07-31 00:38:22 -0300

    Individual category screen

--- components/ui/currency.tsx
+++ components/ui/currency.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 interface CurrencyProps{
   value?: string | number;
 }
@@ -10,6 +14,16 @@ const formatter = new Intl.NumberFormat("pt-BR", {
 const Currency: React.FC<CurrencyProps> = ({
   value
 }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if(!isMounted) {
+    return null;
+  }
+  
   return (
     <div className="font-semibold">
       {formatter.format(Number(value))}
